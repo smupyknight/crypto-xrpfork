4ab836d3f34ed700a938d027c9c483d7dd5ccfc0
smupyknight <smupyknight@outlook.com>
2018-02-06 15:48:50 +0800

    Initial build and commit

---
+++ fork_build/CMakeFiles/rippled.dir/DependInfo.cmake
@@ -0,0 +1,150 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/ed25519_donna.c" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/ed25519_donna.c.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/lz4.c" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/lz4.c.o"
+  "/Volumes/Project/XRPFork/rippled/src/sqlite/sqlite_unity.c" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/sqlite/sqlite_unity.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "BEAST_COMPILE_OBJECTIVE_CPP=1"
+  "BOOST_ASIO_HAS_STD_ARRAY"
+  "BOOST_NO_AUTO_PTR"
+  "DEBUG"
+  "DEPRECATED_IN_MAC_OS_X_VERSION_10_7_AND_LATER"
+  "HAVE_USLEEP=1"
+  "OPENSSL_NO_SSL2"
+  "SOCI_CXX_C11=1"
+  "_DEBUG"
+  "_FILE_OFFSET_BITS=64"
+  "_SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/beast"
+  "../src/beast/include"
+  "../src/beast/extras"
+  "../src/nudb/include"
+  "../src/soci/src"
+  "../src/soci/include"
+  "/Volumes/Project/XRPFork/boost_1_65_1"
+  "/usr/local/opt/openssl/include"
+  "/usr/local/include"
+  "."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Volumes/Project/XRPFork/rippled/src/ripple/beast/unity/beast_hash_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/beast/unity/beast_hash_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/beast/unity/beast_insight_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/beast/unity/beast_insight_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/beast/unity/beast_net_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/beast/unity/beast_net_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/beast/unity/beast_utility_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/beast/unity/beast_utility_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/app_consensus.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/app_consensus.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/app_ledger.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/app_ledger.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/app_ledger_impl.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/app_ledger_impl.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/app_main1.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/app_main1.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/app_main2.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/app_main2.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/app_misc.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/app_misc.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/app_misc_impl.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/app_misc_impl.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/app_paths.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/app_paths.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/app_tx.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/app_tx.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/basics.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/basics.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/beast.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/beast.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/beastobjc.mm" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/beastobjc.mm.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/conditions.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/conditions.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/consensus.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/consensus.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/core.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/core.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/crypto.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/crypto.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/json.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/json.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/ledger.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/ledger.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/net.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/net.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/nodestore.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/nodestore.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/overlay1.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/overlay1.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/overlay2.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/overlay2.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/peerfinder.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/peerfinder.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/protobuf.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/protobuf.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/protocol.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/protocol.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/resource.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/resource.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/ripple.proto.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/ripple.proto.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/rocksdb.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/rocksdb.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/rpcx1.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/rpcx1.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/rpcx2.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/rpcx2.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/secp256k1.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/secp256k1.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/server.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/server.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/shamap.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/shamap.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/snappy.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/snappy.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/soci.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/soci.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/ripple/unity/soci_ripple.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/ripple/unity/soci_ripple.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/app_test_unity1.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/app_test_unity1.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/app_test_unity2.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/app_test_unity2.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/basics_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/basics_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/beast_test_unity1.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/beast_test_unity1.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/beast_test_unity2.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/beast_test_unity2.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/conditions_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/conditions_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/consensus_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/consensus_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/core_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/core_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/csf_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/csf_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/json_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/json_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/jtx_unity1.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/jtx_unity1.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/jtx_unity2.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/jtx_unity2.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/ledger_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/ledger_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/nodestore_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/nodestore_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/overlay_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/overlay_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/peerfinder_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/peerfinder_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/protocol_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/protocol_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/resource_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/resource_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/rpc_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/rpc_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/server_status_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/server_status_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/server_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/server_test_unity.cpp.o"
+  "/Volumes/Project/XRPFork/rippled/src/test/unity/shamap_test_unity.cpp" "/Volumes/Project/XRPFork/rippled/fork_build/CMakeFiles/rippled.dir/src/test/unity/shamap_test_unity.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BEAST_COMPILE_OBJECTIVE_CPP=1"
+  "BOOST_ASIO_HAS_STD_ARRAY"
+  "BOOST_NO_AUTO_PTR"
+  "DEBUG"
+  "DEPRECATED_IN_MAC_OS_X_VERSION_10_7_AND_LATER"
+  "HAVE_USLEEP=1"
+  "OPENSSL_NO_SSL2"
+  "SOCI_CXX_C11=1"
+  "_DEBUG"
+  "_FILE_OFFSET_BITS=64"
+  "_SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/beast"
+  "../src/beast/include"
+  "../src/beast/extras"
+  "../src/nudb/include"
+  "../src/soci/src"
+  "../src/soci/include"
+  "/Volumes/Project/XRPFork/boost_1_65_1"
+  "/usr/local/opt/openssl/include"
+  "/usr/local/include"
+  "."
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/Volumes/Project/XRPFork/rippled/fork_build/ripple.pb.h" "/Volumes/Project/XRPFork/rippled/fork_build/ripple.pb.cc"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
